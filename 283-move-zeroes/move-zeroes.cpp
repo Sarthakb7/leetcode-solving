ebcbd6f4f18004fe693fa8688b85587f84019d15
Bhosale Sarthak Sharad <sarthak2336@gmail.com>
2026-08-11 22:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (19.89%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i]==0){
+            continue;
+        } else {
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            j++;
+
+        }
+
+    }
+    }
+};
